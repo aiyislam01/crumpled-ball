fe0625f3ddfcdd676f9e75b68b611332504f08c4
aiyislam01 <83931946+aiyislam01@users.noreply.github.com>
2021-06-21 17:16:36 -0700

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,30 @@
+class ball{
+	constructor(x,y,r)
+	{
+        var options={
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+		this.x=x;
+		this.y=y;
+        this.r=r;
+		
+		this.image=loadImage("paper.png")
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		
+		World.add(world, this.body);
+
+	}
+	display(){
+        var paperpos=this.body.position; 
+        push() 
+        translate(paperpos.x, paperpos.y);  
+        fill(255,0,255) 
+        imageMode(CENTER); 
+        image(this.image, 0,0,this.r, this.r) 
+        pop()
+			
+	}
+
+}
